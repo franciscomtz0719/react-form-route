f69ea321f9b66b1df70e1b63d3ea7067cdbd27a5
franciscomtz0719 <franciscomtz0719@gmail.com>
2022-07-29 16:42:40 -0500

    reat router dom example

---
+++ src/Pages/Contenido/index.js
@@ -0,0 +1,5 @@
+const Contenido = () => {
+    return <h1>Contenido</h1>
+  }
+  
+  export default Contenido
---
+++ src/Pages/NuevoContenido/index.js
@@ -0,0 +1,8 @@
+// import CustomCard from "../../Components/CustomCard"
+
+const NuevoContenido = () => {
+    return (<h1>Nuevo contenido</h1>)
+    
+  }
+  
+  export default NuevoContenido
